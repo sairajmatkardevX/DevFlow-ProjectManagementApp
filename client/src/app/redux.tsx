5ded1a4715e7cacf941ab7a0113884a09e75ccfb
Sairaj Matkar <sairajmatkar.dev@gmail.com>
2025-10-24 18:27:44 +0530

    Improve dashboard data handling

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -1,28 +1,36 @@
-
 "use client";
 
+import { useRef } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store } from "./store"; 
 import { persistStore } from "redux-persist";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
-
-const persistor = persistStore(store);
-setupListeners(store.dispatch);
-
-
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const persistorRef = useRef<any>(null);
+  
+  if (!persistorRef.current) {
+    persistorRef.current = persistStore(store);
+    setupListeners(store.dispatch);
+  }
+
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate 
+        loading={
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div>
+          </div>
+        } 
+        persistor={persistorRef.current}
+      >
         {children}
       </PersistGate>
     </Provider>
   );
 }
-
