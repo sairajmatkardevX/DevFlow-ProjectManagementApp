758e9aeed7a7736031bffdff9e02b25920702077
Sairaj Matkar <sairajmatkar.dev@gmail.com>
2025-10-30 14:01:49 +0530

    fix: resolve React 310 error by fixing Redux persistence setup

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "../app/SessionProvider" 
 import SessionProvider from "./SessionProvider";
 import StoreProvider from "./redux";
 import { ThemeProvider } from "@/components/theme-provider";
